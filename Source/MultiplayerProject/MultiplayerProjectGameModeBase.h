245be9f86998da3bb2e7014d91d348af28c1c765
Pavel <pasha.splav22@gmail.com>
2021-04-12 13:49:44 +0700

    Add project

---
+++ Source/MultiplayerProject/MultiplayerProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiplayerProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERPROJECT_API AMultiplayerProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
